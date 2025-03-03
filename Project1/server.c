0a1a8ec0253473b2298e84e8686f9d95fab2bd57
Darin755 <darinrawson@gmail.com>
2025-03-03 11:57:29 -0700

    added bind

--- Project1/server.c
+++ Project1/server.c
@@ -7,9 +7,23 @@
 #define PORT 80
 
 int main() {
+    //define our buffers and vars
+    struct sockaddr_in serveraddr;
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(80);
+
     //open it
     int listensock = socket(AF_INET, SOCK_STREAM, 0);
     puts("created socket");
+
+    bind(listensock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+    puts("created bind");
+
+    if(listen(listensock, 10) == -1) {
+        puts("failed to listen");
+        return -1;
+    }
 
     //close it
     close(listensock);
