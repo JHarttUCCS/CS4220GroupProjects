1ca0ba5a1093195e202f40314bec30ce2cf0895a
ParadigmMango2 <jacobjhartt@gmail.com>
2025-03-06 13:07:01 -0700

    Added comments explaining the current client source code

--- Project1/client.c
+++ Project1/client.c
@@ -1,27 +1,40 @@
+// Jacob Hartt, Derin Rawson, Lea Karsanbhai
+// Serena Sullivan
+// CS4220.002
+// 04/17/2025
+
+#define PORT 80 // http port
+#define MAX_PENDING_CONNECTIONS 10
+
+
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#define PORT 80
+
 
 int main() {
     //define our buffers and vars
-    struct sockaddr_in serveraddr;
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serveraddr.sin_port = htons(80);
+    struct sockaddr_in clientaddr; // sockaddr_in is an internet address
+    clientaddr.sin_family = AF_INET; // AF_INET means this is basically an IPv4 socket with a port #
+    clientaddr.sin_addr.s_addr = htonl(INADDR_ANY); // htonl() converts a host integer to a
+													// network long (sometimes differnt byte order)
+    clientaddr.sin_port = htons(PORT); // htons() converts a host integer to a network 
+									   // short (sometimes differnt byte order)
 
     //open it
-    int listensock = socket(AF_INET, SOCK_STREAM, 0);
+    int listensock = socket(AF_INET, SOCK_STREAM, 0); // open an IPv4 socket for a stream connection 
+													  // with the default protocol 
     puts("created socket");
 
-    bind(listensock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+    bind(listensock, (struct sockaddr*)&clientaddr, sizeof(clientaddr)); // bind the socket to the clientaddress
     puts("created bind");
 
-    if(listen(listensock, 10) == -1) {
-        puts("failed to listen");
+    if(listen(listensock, MAX_PENDING_CONNECTIONS) == -1) { // attempt to start listening to the socket,
+															// with a maximum of 10 pending connections
+        puts("failed to start listening");
         return -1;
     }
 
@@ -29,3 +42,4 @@ int main() {
     close(listensock);
     puts("closed socket");
 }
+
