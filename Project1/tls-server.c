4f9b7291c93f360b72ffaccec659660037c6772e
ParadigmMango2 <jacobjhartt@gmail.com>
2025-03-11 10:48:23 -0600

    Update tls to two files and makefile

---
+++ Project1/tls-server.c
@@ -0,0 +1,25 @@
+// Jacob Hartt, Derin Rawson, Lea Karsanbhai
+// Serena Sullivan
+// CS4220.002
+// 04/17/2025
+
+#include <stdio.h>
+
+// #include <openssl/bio.h>
+#include <openssl/ssl.h>
+// #include <openssl/err.h>
+
+
+int main() {
+	// Initialize SSL with no options, no settings
+	if (!OPENSSL_init_ssl(0, NULL)) {
+        fprintf(stderr, "Failed to initialize OpenSSL\n");
+        return 1;
+    } else {
+		printf("Successfully initialized OpenSSL\n");
+	}
+
+	printf("TLS (Not)\n");
+
+	return 0;
+}
