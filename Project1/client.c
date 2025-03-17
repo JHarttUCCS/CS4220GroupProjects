5530702428bb6a2dfd45ec40dfaa65394a58cd8b
Darin755 <darinrawson@gmail.com>
2025-03-17 11:02:05 -0600

    basic client going

--- Project1/client.c
+++ Project1/client.c
@@ -10,22 +10,26 @@ int main() {
     //define our buffers and vars
     struct sockaddr_in serveraddr;
     serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serveraddr.sin_port = htons(80);
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serveraddr.sin_port = htons(PORT);
 
     //open it
     int listensock = socket(AF_INET, SOCK_STREAM, 0);
     puts("created socket");
 
-    bind(listensock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
-    puts("created bind");
-
-    if(listen(listensock, 10) == -1) {
-        puts("failed to listen");
-        return -1;
+    if (connect(listensock, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) == 0){
+        puts("connected");
+    } else {
+        puts("failed")
+        return -1
     }
+    
+    //send GET
+
+    
 
     //close it
     close(listensock);
     puts("closed socket");
 }
+
