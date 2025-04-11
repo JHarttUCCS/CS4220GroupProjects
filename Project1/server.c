c2d51be932c4600e982fcc2e3fccecad140037e0
lkarsanb <lkarsanb@uccs.edu>
2025-04-10 21:22:28 -0600

    Single connection http server.

--- Project1/server.c
+++ Project1/server.c
@@ -1,28 +1,55 @@
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdlib.h>
 
 #define ISVALIDSOCKET(s) ((s) >= 0)
 #define CLOSESOCKET(s) close(s)
 #define GETSOCKETERRNO() (errno)
 #define PORT "8080"
+#define BUFSIZE 1024
+typedef int SOCKET;
 
+SOCKET create_socket(const char* host, const char *port);
+void handle_connection (SOCKET socket_client);
 
-void handleConnection(int listening_socket){
-    puts("connection established");
-}
 
 int main(void)
 {
-    typedef int SOCKET;
+    SOCKET listen_socket = create_socket(0, PORT);
 
+    /* Accept any incoming connection. */
+    printf("Waiting for connection...\n");
+    struct sockaddr_storage client_address;
+    socklen_t client_len = sizeof(client_address);
+    SOCKET socket_client = accept(listen_socket, (struct sockaddr *)&client_address, &client_len);
+
+    if (!ISVALIDSOCKET(socket_client))
+    {
+        fprintf(stderr, "accept() failed. (%d)\n", GETSOCKETERRNO());
+        return 1;
+    }
+
+    handle_connection(socket_client);
+
+    //Close the client connection
+    printf("Closing connection...\n");
+    CLOSESOCKET(socket_client);
+
+    printf("\nClosing socket...\n");
+    CLOSESOCKET(listen_socket);
+
+    printf("Finished.\n");
+    return 0;
+}
+
+SOCKET create_socket(const char* host, const char *port)
+{
     /* Find local address that web server should bind to. */
     printf("Configuring local addresses...\n");
     struct addrinfo loc_address;
@@ -32,8 +59,11 @@ int main(void)
     loc_address.ai_flags = AI_PASSIVE;
 
     struct addrinfo *bind_address;
-    getaddrinfo(0, PORT, &loc_address, &bind_address);
-
+    if (getaddrinfo(0, PORT, &loc_address, &bind_address))
+    {
+        fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
 
     /* Create the listening socket that will accept new connections. */
     printf("Creating socket...\n");
@@ -43,15 +73,15 @@ int main(void)
     if (!ISVALIDSOCKET(listening_socket))
     {
         fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
-        return 1;
+        exit(1);
     }
 
-    /* Use bind() to associate the socket with the address from getaddrinfo() */
+    /* Use bind() to associate the socket with the address from getaddrinfo(). */
     printf("Binding socket to local address...\n");
     if (bind(listening_socket, bind_address->ai_addr, bind_address->ai_addrlen))
     {
         fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
-        return 1;
+        exit(1);
     }
 
     freeaddrinfo(bind_address); // Release the address memory
@@ -61,32 +91,45 @@ int main(void)
     if (listen(listening_socket, 5) < 0)
     {
         fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
-        return 1;
+        exit(1);
     }
 
-    /* Accept any incoming connection. */
-    printf("Waiting for connection...\n");
-    struct sockaddr_storage client_address;
-    socklen_t client_len = sizeof(client_address);
-    SOCKET socket_client = accept(listening_socket, (struct sockaddr *)&client_address, &client_len);
+    return listening_socket;
+}
 
-    if (!ISVALIDSOCKET(socket_client))
+/* Receive and send data on the connection. */
+void handle_connection (SOCKET client_socket)
+{
+    // Read client request.
+    printf("Reading request...\n");
+    char request[BUFSIZE];
+    int bytes_received = recv(client_socket, request, BUFSIZE, 0);
+
+    // Check that recv() > 0
+    if (bytes_received <= 0)
     {
-        fprintf(stderr, "accept() failed. (%d)\n", GETSOCKETERRNO());
-        return 1;
-    } else {
-        //code to process the connection
-        handleConnection(listening_socket);
+        fprintf(stderr, "recv() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
     }
+    printf("Received %d bytes. \n", bytes_received);
 
+    // Display request to the console.
+    printf("%.*s", bytes_received, request);
 
-    //Close the client connection
-    printf("Closing connection...\n");
-    CLOSESOCKET(socket_client);
+    // Server needs to respond back.
+    printf("Sending response...\n");
+    const char *response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Connection: close\r\n"
+        "Server: HTTP Server\r\n\r\n"
+        "Hello from the HTTP server!\n";
 
-    printf("Closing listening socket...\n");
-    CLOSESOCKET(listening_socket);
-
-    printf("Finished.\n");
-    return 0;
+    int bytes_sent = send(client_socket, response, strlen(response), 0);
+    if (bytes_sent < 0)
+    {
+        perror("Error sending data to client");
+        exit(1);
+    }
+    printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(response));
 }
