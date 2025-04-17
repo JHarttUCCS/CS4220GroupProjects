32bdf01ca35bcc8eeab8c8f5f23f375476269357
Darin755 <darinrawson@gmail.com>
2025-04-17 12:32:01 -0600

    finished SSL

--- Project1/client.c
+++ Project1/client.c
@@ -6,12 +6,40 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+#include <arpa/inet.h>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+
+#define PORT 8080
+#define BUFSIZE 1024
+
 int main() {
+
+    //initialize OpenSSL
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+    SSL_CTX *sslctx = SSL_CTX_new(TLS_method());
+    if(sslctx == NULL) {
+        fprintf(stderr, "%s", "Failed to intialize OpenSSL. SSL context returned NULL\n");
+        return -1;
+    } else {
+        puts("Loaded OpenSSL");
+    }
+
+    if(SSL_CTX_set_min_proto_version(sslctx, TLS1_2_VERSION) != 1) {
+        fprintf(stderr, "%s", "Failed to set min TLS to 1.2\n");
+        return -1;
+    }
+
+    //fireup SSL
+    SSL *sslclient = SSL_new(sslctx);
+
     //define our buffers and vars
     struct sockaddr_in serveraddr;
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serveraddr.sin_port = htons(8000);
+    serveraddr.sin_port = htons(PORT);
 
     //open it
     int listensock = socket(AF_INET, SOCK_STREAM, 0);
@@ -23,32 +51,45 @@ int main() {
         puts("failed");
         return -1;
     }
+
+    //config SSL to use socket
+    SSL_set_connect_state(sslclient);
+
+    if(SSL_set_fd(sslclient, listensock) != 1) {
+        fprintf(stderr, "%s\n", "SSL failed to bind to socket fd");
+        return -1;
+    }
     
-    //500 byte buffer
-    char *tcpbuff = (char*) malloc(10000);
+    //byte buffer
+    char *tcpbuff = (char*) malloc(BUFSIZE);
 
     //GET seg string 
-    strcpy(tcpbuff, "GET  / HTTP/1.1\r\n";
-        "Host: 127.0.0.1:8000\r\n";
-        "User-Agent: SimpleHttpClient/1.0\r\n";
-        "Accept: text/html\r\n";
-        "Accept-Language:en-US\r\n";
+    strcpy(tcpbuff, "GET / HTTP/1.1\r\n"
+        "Host: 127.0.0.1:8000\r\n"
+        "User-Agent: SimpleHttpClient/1.0\r\n"
+        "Accept: text/html\r\n"
+        "Accept-Language:en-US\r\n"
         "Accept-Encoding: identity\r\n\r\n");
     
     //send it and zero
-    write(listensock, tcpbuff, 10000);
-    bzero(tcpbuff, 500);
+    SSL_write(sslclient, tcpbuff, BUFSIZE);
+    bzero(tcpbuff, BUFSIZE);
 
     //read reply
-    read(listensock, tcpbuff, 10000);
+    SSL_read(sslclient, tcpbuff, BUFSIZE);
 
-    //print reply
-    printf("\n%s\n", tcpbuff);
+    //remove headers
+    char *q = strstr(tcpbuff, "\r\n\r\n")+4;
+    if(q) {
+        //print reply
+        printf("%s", q);
+    }
 
     //free mem
     free(tcpbuff);
 
     //close it
+    SSL_shutdown(sslclient);
     close(listensock);
     puts("Good bye");
 }
--- Project1/server.c
+++ Project1/server.c
@@ -21,11 +21,10 @@
 typedef int SOCKET;
 
 SOCKET create_socket(const char* host, const char *port);
-void handle_connection (SOCKET socket_client);
+int handle_connection (SSL *client_ssl);
 
 int main(void)
 {
-    SOCKET listen_socket = create_socket(0, PORT);
 
     //initialize OpenSSL
     SSL_library_init();
@@ -35,57 +34,91 @@ int main(void)
     if(sslctx == NULL) {
         fprintf(stderr, "%s", "Failed to intialize OpenSSL. SSL context returned NULL\n");
         return -1;
+    } else {
+        puts("Loaded OpenSSL");
     }
-    //SSL_CTX_set_min_proto_version
+
+    if(SSL_CTX_set_min_proto_version(sslctx, TLS1_2_VERSION) != 1) {
+        fprintf(stderr, "%s", "Failed to set min TLS to 1.2\n");
+        return -1;
+    }
     
-    if (SSL_CTX_use_certificate_file(sslctx, "keys/server.crt", SSL_FILETYPE_PEM) != 1) {
-        fprintf(stderr, "%s", "Failed to load keys\n");
-        return 1;
+    if(SSL_CTX_use_PrivateKey_file(sslctx, "keys/server.key", SSL_FILETYPE_PEM) != 1) {
+        fprintf(stderr, "%s", "Failed to load private key\n");
+        return -1;
+    } else {
+        puts("loaded private key");
     }
+
+    if (SSL_CTX_use_certificate_file(sslctx, "keys/server.crt", SSL_FILETYPE_PEM) != 1) {
+        fprintf(stderr, "%s", "Failed to load public key\n");
+        return -1;
+    } else {
+        puts("Loaded public key");
+    }
+
+    //fireup SSL
+    SSL *sslclient = SSL_new(sslctx);
+
+    //establish socket connection
+    SOCKET listen_socket = create_socket(0, PORT);
+
 
     /* Accept any incoming connection. */
     printf("Waiting for connection...\n");
     struct sockaddr_storage client_address;
     socklen_t client_len = sizeof(client_address);
     SOCKET socket_client = accept(listen_socket, (struct sockaddr *)&client_address, &client_len);
+    //config SSL to use socket
+    SSL_set_accept_state(sslclient);
+
+    if(SSL_set_fd(sslclient, socket_client) != 1) {
+        fprintf(stderr, "%s\n", "SSL failed to bind to socket fd");
+        return -1;
+    }
 
     if (!ISVALIDSOCKET(socket_client))
     {
         fprintf(stderr, "accept() failed. (%d)\n", GETSOCKETERRNO());
-        return 1;
+        return -1;
+    }
+
+    //do client handshake
+    if (SSL_do_handshake(sslclient) != 1) {
+        fprintf(stderr, "%s", "SSL handshake failed\n");
+        return -1;
     }
 
     /* Read client request. */
     printf("Reading request...\n");
-    char request[BUFSIZE];
-    int bytes_received = recv(socket_client, request, BUFSIZE, 0);
-
-    // Check that recv() > 0
-    if (bytes_received <= 0)
-    {
-        fprintf(stderr, "recv() failed. (%d)\n", GETSOCKETERRNO());
-        exit(1);
+    char *request = (char*) malloc(BUFSIZE);
+    //int bytes_received = recv(socket_client, request, BUFSIZE, 0);
+    size_t bytesRead = 0;
+    if (SSL_read_ex(sslclient, request, BUFSIZE, &bytesRead) != 1) {
+        fprintf(stderr, "%s", "SSL_read Failed\n");
+        return -1;
     }
 
-    // Detect if an HTTP header has been received.
-    else
+    request[bytesRead] = 0;
+    char *q = strstr(request, "\r\n\r\n");
+    if (q)
     {
-        request[bytes_received] = 0;
-        char *q = strstr(request, "\r\n\r\n");
-        if (q)
-        {
             // Ensure that it is a GET request.
-            if (strncmp("GET /", request, 5))
-            {
-                fprintf(stderr, "Bad request. (%s)\n", request);
-                exit(1);
-            }
-
-            // Rend message back to client.
-            handle_connection(socket_client);
+        if (strncmp("GET /", request, 5))
+        {
+            fprintf(stderr, "Bad request. (%s)\n", request);
+            return -1;
         }
+
+        // Send message back to client and handle errors with program crash
+        if(handle_connection(sslclient) != 1) {
+            return -1;
+        } 
     }
 
+    //shutdown SSL
+    puts("Closing SSL");
+    SSL_shutdown(sslclient);
 
     //Close the client connection
     printf("Closing connection...\n");
@@ -93,6 +126,10 @@ int main(void)
 
     printf("\nClosing socket...\n");
     CLOSESOCKET(listen_socket);
+
+    puts("Freeing memory");
+    //free mem
+    free(request);
 
     printf("Finished.\n");
     return 0;
@@ -112,7 +149,7 @@ SOCKET create_socket(const char* host, const char *port)
     if (getaddrinfo(0, PORT, &loc_address, &bind_address))
     {
         fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
-        exit(1);
+        return -1;
     }
 
     /* Create the listening socket that will accept new connections. */
@@ -123,7 +160,7 @@ SOCKET create_socket(const char* host, const char *port)
     if (!ISVALIDSOCKET(listening_socket))
     {
         fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
-        exit(1);
+        return -1;
     }
 
     /* Use bind() to associate the socket with the address from getaddrinfo(). */
@@ -131,7 +168,7 @@ SOCKET create_socket(const char* host, const char *port)
     if (bind(listening_socket, bind_address->ai_addr, bind_address->ai_addrlen))
     {
         fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
-        exit(1);
+        return -1;
     }
 
     freeaddrinfo(bind_address); // Release the address memory
@@ -141,14 +178,14 @@ SOCKET create_socket(const char* host, const char *port)
     if (listen(listening_socket, 5) < 0)
     {
         fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
-        exit(1);
+        return -1;
     }
 
     return listening_socket;
 }
 
 /* Receive and send data on the connection. */
-void handle_connection (SOCKET client_socket)
+int handle_connection (SSL *client_ssl)
 {
     /* Respond to client from server. */
     printf("Sending response...\n");
@@ -156,14 +193,16 @@ void handle_connection (SOCKET client_socket)
         "HTTP/1.1 200 OK\r\n"
         "Content-Type: text/plain\r\n"
         "Connection: close\r\n"
-        "Server: HTTP Server\r\n\r\n"
-        "Hello from the HTTP server!\n";
+        "Server: HTTPS Server\r\n"
+        "Content-Length: 29\r\n\r\n"
+        "Hello from the HTTPS server!\n";
 
-    int bytes_sent = send(client_socket, response, strlen(response), 0);
+    int bytes_sent = SSL_write(client_ssl, response, strlen(response));
     if (bytes_sent < 0)
     {
         perror("Error sending data to client");
-        exit(1);
+        return -1;
     }
     printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(response));
+    return 1;
 }
